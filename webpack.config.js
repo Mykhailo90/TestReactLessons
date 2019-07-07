9b2f74bc8b80525f68faadc3f02ac088e76eef77
msarapii <mykhailosarapii@gmail.com>
2019-07-07 22:30:35 +0300

    tetris in js

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,40 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+const CSSModuleLoader = {
+  loader: 'css-loader',
+  options: {
+    modules: true,
+    sourceMap: true,
+    localIdentName: '[local]__[hash:base64:5]',
+    minimize: true
+  }
+}
+
+const CSSLoader = {
+  loader: 'css-loader',
+  options: {
+    modules: false,
+    sourceMap: true,
+    minimize: true
+  }
+}
+
+const postCSSLoader = {
+  loader: 'postcss-loader',
+  options: {
+    ident: 'postcss',
+    sourceMap: true,
+    plugins: () => [
+      autoprefixer({
+        browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9']
+      })
+    ]
+  }
+}
+
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/app/App.js",
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "index_bundle.js"
@@ -20,6 +52,20 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /\.module\.scss$/,
+        use: ['style-loader', CSSLoader, postCSSLoader, 'sass-loader']
+      },
+      {
+        test: /\.module\.scss$/,
+        use: [
+          'style-loader',
+          CSSModuleLoader,
+          postCSSLoader,
+          'sass-loader',
+        ]
       }
     ]
   },
